870e3cfc50b14015cafdb25d9cb706378675e29d
RafaPinzon <rafapinzon_@hotmail.com>
2015-09-04 14:05:27 -0500

    actualizacion con el for

--- Suma_Vectores_CUD.c
+++ Suma_Vectores_CUD.c
@@ -8,7 +8,7 @@ Rafael Pinzón Rivera 1088313004
 #include <time.h>
 #include <math.h>
 #define SIZE 2000
-#define BLOCKSIZE 1024
+#define BLOCKSIZE 1
 
 __global__ void vecAdd(int *A, int *B, int *C, int n){
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
@@ -17,7 +17,7 @@ __global__ void vecAdd(int *A, int *B, int *C, int n){
 			//blockIdx.x;
   if (i < n){
 		C[i] = A[i] + B[i];
-	    printf("%d. %d + %d = %d\n",i, A[i], B[i], C[i]);
+	    // printf("%d. %d + %d = %d\n",i, A[i], B[i], C[i]);
   }
 }
 int vectorAddGPU( int *A, int *B, int *C, int n){
@@ -32,7 +32,7 @@ int vectorAddGPU( int *A, int *B, int *C, int n){
 	cudaMemcpy(d_B, B, size, cudaMemcpyHostToDevice);
 	// Ejecuto el Kernel (del dispositivo)
 	int dimGrid = ceil(SIZE/BLOCKSIZE);
-	printf("%d\n", dimGrid);
+	printf(" DimGrid %d\n", dimGrid);
 	vecAdd<<< dimGrid, BLOCKSIZE >>>(d_A, d_B, d_C, n);
 	// vecAdd<<< HILOS, BLOCKES >>>
 	cudaMemcpy(C, d_C, size, cudaMemcpyDeviceToHost);
@@ -46,38 +46,43 @@ int vectorAddCPU( int *A, int *B, int *C, int n){
 	int i;
 	for(i=0;i< n; i++){
 		C[i]=A[i]+B[i];
-		printf("%d. %d+", i, A[i]);
-		printf("%d=",B[i]);
-		printf("%d\n",C[i]);
+		//printf("%d. %d+", i, A[i]);
+    //printf("%d=",B[i]);
+		//printf("%d\n",C[i]);
 	}
 	return 0;
 }
 
 int main(){
-	int *A=(int *) malloc(SIZE*sizeof(int));
-	int *B=(int *) malloc(SIZE*sizeof(int));
-	int *C=(int *) malloc(SIZE*sizeof(int));
-	time_t inicioCPU, inicioGPU,finCPU, finGPU;
-	int i;
-	for(i=0;i< SIZE; i++){
-		A[i]=rand()%21;
-		B[i]=rand()%21;
-		// A[i]=srand(time(NULL));
-		// B[i]=srand(time(NULL));
-	
-	}
-	// Ejecuto por GPU
-	inicioGPU=time(NULL);
-	vectorAddGPU(A, B, C, SIZE);
-	finGPU = time(NULL);
-	// Ejecuto por CPU
-	inicioCPU=time(NULL);
-	vectorAddCPU(A, B, C, SIZE);
-	finCPU=time(NULL);
+	int j;
+	int SIZES[] = {512, 1024, 3000, 5000, 1000000, 5000000};
+	for (j = 0; j < sizeof(SIZES)/sizeof(SIZES[0]); ++j)
+	{
+		int *A=(int *) malloc(SIZES[j]*sizeof(int));
+		int *B=(int *) malloc(SIZES[j]*sizeof(int));
+		int *C=(int *) malloc(SIZES[j]*sizeof(int));
+		clock_t inicioCPU, inicioGPU,finCPU, finGPU;
+		int i;
+		for(i=0;i< SIZES[j]; i++){
+			A[i]=rand()%21;
+			B[i]=rand()%21;
+			// A[i]=srand(time(NULL));
+			// B[i]=srand(time(NULL));
 
-	printf("El tiempo GPU es: %f\n",difftime(finGPU,inicioGPU));
-	printf("El tiempo CPU es: %f\n",difftime(finCPU,inicioCPU));
-	free(A);
-	free(B);
+		}
+		// Ejecuto por GPU
+		inicioGPU=clock();
+		vectorAddGPU(A, B, C, SIZES[j]);
+		finGPU = clock();
+		// Ejecuto por CPU
+		inicioCPU=clock();
+		vectorAddCPU(A, B, C, SIZES[j]);
+		finCPU=clock();
+		printf("Size %d\n", SIZES[j]);
+		printf("El tiempo GPU es: %f\n",(double)(finGPU - inicioGPU) / CLOCKS_PER_SEC);
+		printf("El tiempo CPU es: %f\n",(double)(finCPU - inicioCPU) / CLOCKS_PER_SEC);
+		free(A);
+		free(B);
+	}
 	return 0;
 }
